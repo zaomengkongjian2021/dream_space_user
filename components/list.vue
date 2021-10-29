53c326559d088bc24186326c599916d93f2ecc25
Tomcat_Win <724495690@qq.com>
2021-10-29 23:05:35 +0800

    init commit

---
+++ components/list.vue
@@ -0,0 +1,34 @@
+<template>
+	<view>
+		<uni-list>
+			<uni-list-item v-for="(item, index) in list" :key="index" :title="item.title" :note="item.note" @click="handleItem" :link="item.isLink" :to="item.to">
+			</uni-list-item>
+		</uni-list>
+	</view>
+</template>
+
+<script>
+	export default {
+		name: "list",
+		props: {
+			list: {
+				type: Array,
+				required: true
+			}
+		},
+		data() {
+			return {
+
+			};
+		},
+		methods: {
+			handleItem (...rest) {
+				console.log('@@@@@@@@@@', rest);
+			}
+		}
+	}
+</script>
+
+<style>
+
+</style>
--- pages/myCenter/myCenter.vue
+++ pages/myCenter/myCenter.vue
@@ -18,43 +18,130 @@
 			<el-col :span="24" class="center-title">我的订单</el-col>
 			<el-col :span="24" class="order-label">
 				<p>
-					<i class="el-icon-tickets"></i><span>待付</span><a>0</a>
+					<el-badge :value="1" :max="99">
+						<i class="el-icon-tickets"></i>
+					</el-badge>
+					<span>待付</span>
 				</p>
 				<p>
-					<i class="el-icon-news"></i><span>待发</span><a>0</a>
+					<el-badge :value="10" :max="99">
+						<i class="el-icon-news"></i>
+					</el-badge>
+					<span>待发</span>
 				</p>
 				<p>
-					<i class="el-icon-document"></i><span>待收</span><a>0</a>
+					<el-badge :value="0" :max="99">
+						<i class="el-icon-document"></i>
+					</el-badge>
+					<span>待收</span>
 				</p>
 				<p>
-					<i class="el-icon-goods"></i><span>待还</span><a>0</a>
+					<el-badge :value="1" :max="99">
+						<i class="el-icon-goods"></i>
+					</el-badge>
+					<span>待还</span>
 				</p>
 				<p>
-					<i class="el-icon-date"></i><span>已还</span><a>0</a>
+					<el-badge :value="10" :max="99">
+						<i class="el-icon-date"></i>
+					</el-badge>
+					<span>已还</span>
 				</p>
 			</el-col>
 		</el-row>
 		<el-row class="my-other">
 			<el-col>
-				<span>我的代币券</span><i class="el-icon-arrow-right"></i>
+				<div>
+					<span>我的代币券</span><i class="el-icon-arrow-right"></i>
+				</div>
 			</el-col>
 			<el-col>
-				<span>推荐历史</span><i class="el-icon-arrow-right"></i>
+				<div @click="viewMyPrize">
+					<span>我的奖品</span><i class="el-icon-arrow-right"></i>
+				</div>
 			</el-col>
 			<el-col>
-				<span>造梦联盟</span><i class="el-icon-arrow-right"></i>
+				<div @click="commendRecord">
+					<span>推荐历史</span><i class="el-icon-arrow-right"></i>
+				</div>
+			</el-col>
+			<el-col v-if="isVIP">
+				<div @click="forwardUnionArea">
+					<span>造梦联盟</span><i class="el-icon-arrow-right"></i>
+				</div>
 			</el-col>
 			<el-col>
-				<span>联系客服</span><i class="el-icon-arrow-right"></i>
+				<div @click="contactUs">
+					<span>联系客服</span><i class="el-icon-arrow-right"></i>
+				</div>
 			</el-col>
-			<el-col style="border-bottom: none;">
-				<span>退出登录</span><i class="el-icon-arrow-right"></i>
+			<el-col>
+				<div @click="beforeLoginOut" style="border-bottom: none;">
+					<span>退出登录</span><i class="el-icon-arrow-right"></i>
+				</div>
 			</el-col>
 		</el-row>
 	</view>
 </template>
 
 <script>
+	export default {
+		data() {
+			return {
+				user: {},
+
+			}
+		},
+		computed: {
+			isVIP() {
+				return this.user && this.user.user_type === 'vip'
+			}
+		},
+		created() {
+			this.user = this.getUserInfo()
+		},
+		methods: {
+			getUserInfo () {
+				return JSON.parse(sessionStorage.getItem("user") || {})
+			},
+			// 查看我的奖品
+			viewMyPrize() {
+				// 去我的奖品列表页面
+				this.$router.push("/pages/myCommend/myCommend")
+			},
+			// 我的推荐
+			commendRecord() {
+				// 去我的推荐人页 
+			},
+			// 去联盟页面
+			forwardUnionArea() {
+				this.$router.push("/pages/unionArea/unionArea")
+			},
+			// 联系我们
+			contactUs() {
+				console.log('拉起微信？')
+				// plus.runtime.openURL("weixin://");
+			},
+			// 登出前提示
+			beforeLoginOut() {
+				// 退出登录前做下提示
+				this.$confirm('是否确认退出应用', '温馨提示', {
+						distinguishCancelAndClose: true,
+						confirmButtonText: '退出',
+						cancelButtonText: '取消'
+					})
+					.then(() => {
+						this.loginOut();
+					})
+					.catch(action => {})
+			},
+			// 退出登录
+			loginOut() {
+				sessionStorage.removeItem('user')
+				this.$router.push("/pages/index/index")
+			}
+		}
+	}
 </script>
 
 <style>
